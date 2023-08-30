9321d7eb80ff53c40a03aa4ba50806e67c385eec
Solomon <solomonosato@gmail.com>
2023-08-30 22:42:09 +0100

    fix code I broke trying to fix images not showing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-	base: '/vite-deploy-demo/',
+	base: '/fem-static-job-listing/',
   }
 
   if (command !== 'serve') {
