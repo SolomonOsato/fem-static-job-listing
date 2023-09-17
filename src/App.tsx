993740eba814f9184b4fed791bc64b213ba3adcc
Solomon <solomonosato18@gmail.com>
2023-09-17 14:12:52 +0100

    remove Jobs import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Jobs } from './assets/data'
 import './App.scss'
 import Job from './components/Job';
 import JobListings from './components/JobListings';
